2b268cac7a0f7a69bf506ea3b8a8fc57fc6e1792
Dominik Straub <dominikstrb@mailbox.org>
2022-07-06 15:22:01 +0200

    Add docstring

--- lqg/lqr.py
+++ lqg/lqr.py
@@ -4,6 +4,18 @@ from lqg.riccati import solve_discrete_riccati
 
 
 def control_law(A, B, Q, R, T):
+    """ LQR control law after T time steps
+
+    Args:
+        A (jnp.array): state transition matrix
+        B (jnp.array): control matrix
+        Q (jnp.array): control costs
+        R (jnp.array): action costs
+        T (jnp.array): time steps
+
+    Returns:
+        jnp.array: LQR controller
+    """
     S = solve_discrete_riccati(A, B, Q, R, T)
 
     L = jnp.linalg.inv(B.T @ S @ B + R) @ B.T @ S @ A
