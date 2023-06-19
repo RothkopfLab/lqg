b6b67c0e8cae51e193d5e58b9b64d694e41c4453
Dominik Straub <dominikstrb@mailbox.org>
2023-06-19 12:11:17 +0200

    Update delay to new API

--- lqg/tracking/delay.py
+++ lqg/tracking/delay.py
@@ -1,51 +1,57 @@
 from jax import numpy as jnp
 from jax.scipy import linalg
 
-from lqg.lqg import System
-from lqg.tracking.subjective import SubjectiveVelocityModel
+from lqg.spec import LQGSpec
+from lqg.lqg import System, Dynamics, Actor
+from lqg.tracking.subjective import SubjectiveActor
+
+
+def delay_system(spec, delay):
+    d = spec.A.shape[1]
+
+    A = jnp.stack([linalg.block_diag(A, jnp.diag(jnp.zeros(d * delay))) + jnp.diag(jnp.ones(d * delay), k=-d)
+                   for A in
+                   spec.A])
+    B = jnp.stack([jnp.vstack([B] + [jnp.zeros_like(B)] * delay) for B in spec.B])
+    F = jnp.stack([jnp.hstack([jnp.zeros((F.shape[0], F.shape[1] * delay)), F]) for F in spec.F])
+
+    V = jnp.stack([linalg.block_diag(V, jnp.diag(jnp.zeros(d * delay))) for V in spec.V])
+
+    Q = jnp.stack([linalg.block_diag(Q, *[jnp.zeros_like(Q)] * delay) for Q in spec.Q])
+
+    return LQGSpec(A=A, B=B, F=F, V=V, W=spec.W, Q=Q, R=spec.R)
 
 
 class TemporalDelayModel(System):
-    def __init__(self, system, delay=3):
-        dyn = system.dynamics
-        d = dyn.A.shape[0]
-
-        A = linalg.block_diag(dyn.A, jnp.diag(jnp.zeros(d * delay))) + jnp.diag(jnp.ones(d * delay), k=-d)
-
-        B = jnp.vstack([dyn.B] + [jnp.zeros_like(dyn.B)] * delay)
-
-        C = jnp.hstack([jnp.zeros((dyn.C.shape[0], dyn.C.shape[1] * delay)), dyn.C])
-
-        V = linalg.block_diag(dyn.V, jnp.diag(jnp.zeros(d * delay)))
-        W = dyn.W
-
-        dyn = Dynamics(A=A, B=B, C=C, V=V, W=W)
-
-        act = system.actor
-        d = act.A.shape[0]
-
-        A = linalg.block_diag(act.A, jnp.diag(jnp.zeros(d * delay))) + jnp.diag(jnp.ones(d * delay), k=-d)
-
-        B = jnp.vstack([act.B] + [jnp.zeros_like(act.B)] * delay)
-
-        C = jnp.hstack([jnp.zeros((act.C.shape[0], act.C.shape[1] * delay)), act.C])
-
-        V = linalg.block_diag(act.V, jnp.diag(jnp.zeros(d * delay)))
-
-        W = act.W
-
-        Q = linalg.block_diag(act.Q, *[jnp.zeros_like(act.Q)] * delay)
-        R = act.R
-        act = Actor(A=A, B=B, C=C, V=V, W=W, Q=Q, R=R)
+    def __init__(self, system, delay):
+        dyn = delay_system(system.dynamics, delay=delay)
+        act = delay_system(system.actor, delay=delay)
 
         super().__init__(actor=act, dynamics=dyn)
 
 
-class DelayedSubjectiveVelocityModel(TemporalDelayModel):
+class DelayedSubjectiveActor(TemporalDelayModel):
     def __init__(self, process_noise=1., c=0.5, motor_noise=0.5, subj_noise=1., subj_vel_noise=10.,
                  sigma=6., prop_noise=3., dt=1. / 60):
-        system = SubjectiveVelocityModel(process_noise=process_noise, c=c, motor_noise=motor_noise,
-                                         subj_noise=subj_noise, subj_vel_noise=subj_vel_noise, sigma=sigma,
-                                         prop_noise=prop_noise, dt=dt)
+        system = SubjectiveActor(process_noise=process_noise, c=c, motor_noise=motor_noise,
+                                 subj_noise=subj_noise, subj_vel_noise=subj_vel_noise, sigma=sigma,
+                                 prop_noise=prop_noise, dt=dt)
 
         super().__init__(system=system, delay=12)
+
+
+if __name__ == '__main__':
+    from jax import random
+    import matplotlib.pyplot as plt
+
+    from lqg import xcorr
+
+    base_model = SubjectiveActor()
+    delayed_model = DelayedSubjectiveActor()
+
+    for model in [base_model, delayed_model]:
+        x = model.simulate(rng_key=random.PRNGKey(0), n=20)
+
+        lags, corrs = xcorr(jnp.diff(x[..., 1], axis=1), jnp.diff(x[..., 0], axis=1))
+        plt.plot(lags, corrs.mean(axis=0))
+    plt.show()
