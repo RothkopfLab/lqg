1793e7d80530a27d16f720996ddc03a7ed56fda3
Dominik Straub <dominikstrb@mailbox.org>
2023-07-20 12:46:27 +0200

    Improve evaluation script

--- lqg/infer/utils.py
+++ lqg/infer/utils.py
@@ -1,11 +1,13 @@
 import jax.numpy as jnp
 from jax import random, lax
+import numpyro
 from numpyro import optim
 from numpyro.infer import NUTS, MCMC, SVI, Trace_ELBO, init_to_median
 from numpyro.infer.autoguide import AutoBNAFNormal
 from numpyro.infer.reparam import NeuTraReparam
 
-from lqg.infer.models import lifted_model as lqg_model
+from lqg.infer.models import lifted_model as lqg_model, get_model_params
+from lqg.infer import prior
 from lqg.tracking import BoundedActor
 
 
@@ -37,3 +39,8 @@ def infer(x, num_samples, num_warmup, model=BoundedActor, numpyro_fn=lqg_model, 
     mcmc.run(random.PRNGKey(seed), x, model, process_noise, dt, **fixed)
 
     return mcmc
+
+
+def sample_from_prior(model_type, seed, prior_dict=prior.default_prior):
+    param_dict = {**numpyro.handlers.seed(prior.sample_params, rng_seed=seed)(prior_dict)}
+    return {k: v for k, v in param_dict.items() if k in get_model_params(model_type).keys()}
--- main.py
+++ main.py
@@ -1,24 +1,65 @@
+import argparse
 import matplotlib.pyplot as plt
 import arviz as az
 from jax import random
+import numpyro
 
-from lqg.tracking import SignalDependentNoiseActor
+numpyro.set_host_device_count(4)
+
+from lqg import tracking
 from lqg.infer import infer
+from lqg.infer.utils import sample_from_prior
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Coverage runs")
+    parser.add_argument("--ntrial", type=int, default=20,
+                        help="Number of trials .")
+    parser.add_argument("--seed", type=int, default=42, help="Seed for the simulation")
+    parser.add_argument("--time", type=int, default=720,
+                        help="Time steps per trial")
+    parser.add_argument("--nsamp", type=int, default=5_000,
+                        help="Number of samples drawn by NUTS")
+    parser.add_argument("--nwarmup", type=int, default=2_500,
+                        help="Number of burn-in samples.")
+    parser.add_argument("--nchain", type=int, default=4,
+                        help="Number of chains.")
+    parser.add_argument("--model", type=str, default="BoundedActor",
+                        help="Model type (lqg.tracking)")
+    parser.add_argument('--plot', action=argparse.BooleanOptionalAction)
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
+    args = parse_args()
+
+    Model = getattr(tracking, args.model)
+
+    params = sample_from_prior(Model, args.seed)
+
     # setup model and simulate data
-    model = SignalDependentNoiseActor(sigma=15., prop_noise=5., motor_noise=0.5, c=0.5, T=500)
-    x = model.simulate(random.PRNGKey(123), n=20)
+    model = Model(T=args.time, **params)
 
-    # visualize trajectories
-    plt.plot(x[0, :, 0])
-    plt.plot(x[0, :, 1])
-    plt.xlabel("time")
-    plt.ylabel("position")
-    plt.show()
+    x = model.simulate(random.PRNGKey(args.seed), n=args.ntrial)
 
-    mcmc = infer(x, model=SignalDependentNoiseActor, num_samples=5_000, num_warmup=2_000)
+    if args.plot:
+        # visualize trajectories
+        plt.plot(x[0, :, 0])
+        plt.plot(x[0, :, 1])
+        plt.xlabel("time")
+        plt.ylabel("position")
+        plt.show()
 
-    data = az.convert_to_inference_data(mcmc)
-    az.plot_pair(data, kind="hexbin")
-    plt.show()
+    mcmc = infer(x, model=Model,
+                 num_samples=args.nsamp, num_warmup=args.nwarmup, num_chains=args.nchain)
+
+    idata = az.convert_to_inference_data(mcmc)
+
+    if args.plot:
+        az.plot_pair(idata)
+        plt.show()
+
+    summary = az.summary(idata)
+    for key in params:
+        summary.loc[key, "true"] = params[key]
+    summary.to_csv(f"results/parameter-recovery/{args.model}-{args.seed}.nc")
