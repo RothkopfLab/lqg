e949d548a5ca1d3f0338ca0f2a73ab77eb3585c9
Dominik Straub <dominikstrb@mailbox.org>
2023-08-30 11:31:18 +0200

    Add __version__ dynamically

--- lqg/__init__.py
+++ lqg/__init__.py
@@ -1,5 +1,7 @@
+import importlib.metadata
+
+__version__ = importlib.metadata.version("lqg")
+
 from lqg.ccg import xcorr
 from lqg.lqg import LQG, Actor, Dynamics, System
 from lqg.spec import LQGSpec
-
-__version__ = "0.2.2"
