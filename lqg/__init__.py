d8f90fabed83b4b0bde2cb2ab8f1668b433acfd8
Dominik Straub <ds2181@cam.ac.uk>
2026-09-22 18:52:27 +0200

    Fix unused imports

--- lqg/__init__.py
+++ lqg/__init__.py
@@ -5,3 +5,12 @@ __version__ = importlib.metadata.version("lqg")
 from lqg.ccg import xcorr
 from lqg.system import LQG, Actor, Dynamics, System
 from lqg.spec import LQGSpec
+
+__all__ = [
+    "xcorr",
+    "LQG",
+    "Actor",
+    "Dynamics",
+    "System",
+    "LQGSpec",
+]
--- lqg/infer/__init__.py
+++ lqg/infer/__init__.py
@@ -1,3 +1,9 @@
 from lqg.infer.utils import infer
 from lqg.infer.mle import max_likelihood
 from lqg.infer.models import get_model_params
+
+__all__ = [
+    "infer",
+    "max_likelihood",
+    "get_model_params",
+]
