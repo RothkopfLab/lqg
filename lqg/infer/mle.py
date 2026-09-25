3315b4a897e71a1076d2af7c828fab564c43a7ef
Dominik Straub <ds2181@cam.ac.uk>
2026-09-25 17:52:24 +0200

    Re-run MLE with restarts

--- lqg/infer/mle.py
+++ lqg/infer/mle.py
@@ -1,23 +1,41 @@
 import jax.numpy as jnp
+import jax
 from jax import random
 import optimistix as optx
 from numpyro.infer.util import log_density
+from numpyro.distributions.transforms import SoftplusTransform
 
-from lqg.tracking import BoundedActor
+from lqg.infer.utils import sample_from_prior
+
+
 from lqg.infer.models import get_model_params, lqg_model
+from lqg.tracking import BoundedActor
+
+params_constraints = {
+    "action_cost": SoftplusTransform(),
+    "sigma_target": SoftplusTransform(),
+    "action_variability": SoftplusTransform(),
+    "signal_dep_noise": SoftplusTransform(),
+    "sigma_cursor": SoftplusTransform(),
+    "sigma": SoftplusTransform(),
+    "subj_noise": SoftplusTransform(),
+    "subj_vel_noise": SoftplusTransform(),
+    "m": SoftplusTransform(),
+    "tau": SoftplusTransform(),
+}
 
 
 def _log_likelihood(log_params, args):
-    numpyro_fn, x, model, process_noise, dt, fixed = args
-    params = {name: jnp.exp(value) for name, value in log_params.items()}
+    numpyro_fn, x, model, fixed = args
+    params = {
+        name: params_constraints[name](value) for name, value in log_params.items()
+    }
     params.update(fixed)
     return -log_density(
         numpyro_fn,
         (x,),
         {
             "model_type": model,
-            "process_noise": process_noise,
-            "dt": dt,
             **fixed,
         },
         params,
@@ -25,69 +43,106 @@ def _log_likelihood(log_params, args):
 
 
 def max_likelihood(
+    key,
     x,
     model=BoundedActor,
     numpyro_fn=lqg_model,
-    process_noise=1.0,
-    dt=1.0 / 60,
     max_steps=2_000,
     **fixed,
 ):
-    initial_params = {
-        name: jnp.log(jnp.asarray(default))
-        for name, default in get_model_params(model).items()
-        if name not in fixed
-    }
-    solver = optx.LBFGS(rtol=1e-3, atol=1e-3)
-    solution = optx.minimise(
-        _log_likelihood,
-        solver,
-        initial_params,
-        args=(numpyro_fn, x, model, process_noise, dt, fixed),
-        max_steps=max_steps,
-        throw=False,
-    )
-    params = {name: jnp.exp(value) for name, value in solution.value.items()}
-    params.update(fixed)
-    # losses = jnp.asarray([solution.stats["f" ]])
+    def _fit(key):
+        initial_params = {
+            name: value
+            for name, value in sample_from_prior(model, seed=key).items()
+            if name not in fixed
+        }
+        solver = optx.LBFGS(rtol=1e-4, atol=1e-4)
+        solution = optx.minimise(
+            _log_likelihood,
+            solver,
+            initial_params,
+            args=(numpyro_fn, x, model, fixed),
+            max_steps=max_steps,
+            throw=False,
+        )
+        params = {
+            name: params_constraints[name](value)
+            for name, value in solution.value.items()
+        }
+        return params, solution.state.f_info.f
+
+    params, nll = jax.vmap(_fit)(random.split(key, 5))
+    params = {k: v[jnp.argmin(nll)] for k, v in params.items()}
 
     return params  # , losses
 
 
 if __name__ == "__main__":
+    jax.config.update("jax_enable_x64", True)
+
     import matplotlib.pyplot as plt
     from tqdm import tqdm
 
-    true_params = dict(
-        action_cost=0.5,
-        action_variability=0.25,
-        sigma_target=8.0,
-        sigma_cursor=2.0,
-    )
-    lqg = BoundedActor(
-        process_noise=1.0,
-        **true_params,
-        T=500,
-    )
+    from lqg.tracking import PointMassBoundedActor
 
-    results = {key: [] for key in true_params}
+    fixed_params = {
+        "process_noise": 1.0,
+        "dt": 1.0 / 60.0,
+        # "sigma_cursor": 12.5,
+        "tau": 0.066,
+    }
+
+    true_values = {
+        key: []
+        for key in get_model_params(PointMassBoundedActor)
+        if key not in fixed_params
+    }
+    results = {
+        key: []
+        for key in get_model_params(PointMassBoundedActor)
+        if key not in fixed_params
+    }
     print("Running maximum likelihood parameter recovery")
     for seed in tqdm(range(100)):
+        # true_params sampled from prior
+        true_params = {
+            name: value
+            for name, value in sample_from_prior(
+                PointMassBoundedActor, seed=seed
+            ).items()
+            if name not in fixed_params
+        }
+        print(f"True parameters: {true_params}")
+
+        lqg = PointMassBoundedActor(
+            **fixed_params,
+            **true_params,
+            T=500,
+        )
+
         x = lqg.simulate(rng_key=random.PRNGKey(seed), n=20)
 
-        params = max_likelihood(x, max_steps=1_000)
+        params = max_likelihood(
+            key=random.PRNGKey(seed),
+            x=x[..., :2],
+            model=PointMassBoundedActor,
+            max_steps=1_000,
+            **fixed_params,
+        )
+        print(f"Estimated parameters: {params}")
 
         for key, item in params.items():
+            true_values[key].append(float(true_params[key]))
             results[key].append(float(item))
 
-    figure, axes = plt.subplots(2, 2, figsize=(10, 8))
-    for axis, (name, estimates) in zip(axes.flat, results.items()):
-        axis.scatter(range(len(estimates)), estimates, alpha=0.7)
-        axis.axhline(
-            true_params[name], color="black", linestyle="--", label="true value"
-        )
+    figure, axes = plt.subplots(2, 3, figsize=(10, 8))
+    for axis, (name, true, estimates) in zip(
+        axes.flat, zip(true_values.keys(), true_values.values(), results.values())
+    ):
+        axis.scatter(true, estimates, alpha=0.7)
+        axis.plot([min(true), max(true)], [min(true), max(true)], "k--", alpha=0.5)
         axis.set_title(name)
-        axis.set_xlabel("simulation seed")
+        axis.set_xlabel("simulated value")
         axis.set_ylabel("estimated value")
         axis.legend()
 
--- lqg/infer/prior.py
+++ lqg/infer/prior.py
@@ -1,7 +1,4 @@
-import numpy as np
 import numpyro
-from jax import numpy as jnp
-from jax.scipy.stats import norm
 from numpyro import distributions as dist
 
 default_prior = {
@@ -19,34 +16,12 @@ default_prior = {
     "sigma_target_3": dist.HalfNormal(50.0),
     "sigma_target_4": dist.HalfNormal(50.0),
     "sigma_target_5": dist.HalfNormal(50.0),
+    "tau": dist.HalfNormal(0.1),
 }
 
 
 def prior():
     return default_prior
-
-
-def lognormal_params(mu, sigma):
-    return np.log(mu**2 / np.sqrt(mu**2 + sigma**2)), np.log(1 + sigma**2 / mu**2)
-
-
-def lognormal_from_quantiles(x1, x2, p1=0.05, p2=0.95):
-    """Compute the parameters of a log-normal distribution, such that F(x1) = p1 and F(x2) = p2
-
-    Args:
-        x1: lower value
-        x2: upper value
-        p1: lower probability
-        p2: upper probability
-
-    Returns:
-        mu, sigma (parameters of the log-normal distribution
-    """
-    sigma = (jnp.log(x2) - jnp.log(x1)) / (norm.ppf(p2) - norm.ppf(p1))
-    mu = (jnp.log(x2) * norm.ppf(p2) - jnp.log(x1) * norm.ppf(p1)) / (
-        norm.ppf(p2) - norm.ppf(p1)
-    )
-    return mu, sigma
 
 
 def sample_params(prior_dict):
